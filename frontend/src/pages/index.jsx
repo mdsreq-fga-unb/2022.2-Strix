be9efb93369b38946174e7538e817f5db954ab16
mv23c <100789090+mv23c@users.noreply.github.com>
2022-12-17 23:28:00 -0300

    Adição do material ui no projeto, configurações e adição de um botão teste na tela inicial do projeto

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,8 +1,16 @@
+import Button from '@mui/material/Button';
+import Head from 'next/head';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>WISE - Página Inicial</title>
+    </Head>
     <div>
       <h1>WISE - Projeto de Requisitos :)</h1>
+      <Button variant="contained">Teste Botão - material ui</Button>
     </div>
+    </>
   )
 }
