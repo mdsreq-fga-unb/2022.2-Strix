8e5ec3822fdb9b3af3e827d8689114bd9a08470e
mv23c <100789090+mv23c@users.noreply.github.com>
2022-12-23 10:02:49 -0300

    Criação do middleware isAuthenticated para controlar as rotas privadas e criação do service e controller de detalhes do usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 // -- Rotas User --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };
