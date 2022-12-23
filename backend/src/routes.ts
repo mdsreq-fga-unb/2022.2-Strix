d0da9d8598cd827d8eac8941029c45d56d9c9ce9
mv23c <100789090+mv23c@users.noreply.github.com>
2022-12-23 11:06:14 -0300

    Adição do service e controller de cadastro de aluno

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { CreateStudentController } from './controllers/student/CreateStudentController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
@@ -12,5 +14,8 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// -- Rotas Student --
+router.post('/students', isAuthenticated, new CreateStudentController().handle)
 
 export { router };
