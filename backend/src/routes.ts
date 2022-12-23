1002eef5839564b1c093c5824471186613ec017c
mv23c <100789090+mv23c@users.noreply.github.com>
2022-12-23 09:09:40 -0300

    Criação do service e controller de autenticação, geração do token jwt

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+
 
 const router = Router();
 
 // -- Rotas User --
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
 
 export { router };
