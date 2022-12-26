a3872c9197556c9270fca69d0335b36588a63ef9
Israel Thalles <santosisrael14@gmail.com>
2022-12-26 18:04:08 -0300

    Adição de tema

---
+++ frontend/src/components/Theme/index.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2196f3'
+        },
+        secondary: {
+            main: '#00bcd4'
+        }
+    }
+})
