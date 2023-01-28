4f3d319a8d74d10b65da955e86867c7d066b4d04
Pedro <pedrodaia.c@gmail.com>
2023-01-28 14:49:19 -0300

    corrigindo erro de tipagem apontado no deploy

--- backend/src/services/student/DetailStudentService.ts
+++ backend/src/services/student/DetailStudentService.ts
@@ -1,44 +1,37 @@
 import prismaClient from "../../prisma";
 
 interface StudentRequest {
-    id: string;
+  id: string;
 }
 
 class DetailStudentService {
-    async execute({ id }: StudentRequest){
-        if(!id) {
-            throw new Error('invalid id');
-        }
-        
-        const student = await prismaClient.student.findFirst({
-            where:{
-                id: id
-            },
-            select:{
-                id: true,
-                name: true,
-                email: true,
-                cpf: true,
-                birthDate: true,
-                phone: true
-            }
-        })
-
-        if (student !== null){
-            return student;
-        }else{
-            throw new Error("There is no student with that id");
-        }
-        
+  async execute({ id }: StudentRequest) {
+    if (!id) {
+      throw new Error("invalid id");
     }
 
-    async getAllWithPendency() {
-        const students = await prismaClient.student.findMany({
-            where: { pendencies: { some: {} } }
-        });
-        return students;
+    const student = await prismaClient.student.findFirst({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        cpf: true,
+        birthDate: true,
+        phone: true,
+      },
+    });
+
+    if (student !== null) {
+      return student;
+    } else {
+      throw new Error("There is no student with that id");
     }
+  }
+
 
 }
 
-export { DetailStudentService }
+export { DetailStudentService };
