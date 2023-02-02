cb0197f51f0e2d629e75839ab9507ae2450f0f69
Pedro Daia Cardoso <pedrodaia.c@gmail.com>
2023-02-01 22:30:00 -0300

    new api route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: "https://strix-backend.onrender.com",
     headers: {
       Authorization: `Bearer ${cookies["@nextauth.token"]}`,
     },
