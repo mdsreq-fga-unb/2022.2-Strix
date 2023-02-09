c1612d6126206d53aaa15c835aafdbb3ab3642f6
Pedro Daia <pedrodaia.c@gmail.com>
2023-02-08 21:44:26 -0300

    formatando páginas de aula e criarAula

--- frontend/src/pages/aulas/index.jsx
+++ frontend/src/pages/aulas/index.jsx
@@ -1,46 +1,46 @@
-import * as React from 'react';
-import { useContext } from 'react';
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { setupAPIClient } from '../../services/api';
-import { canSSRAuth } from '../../../utils/canSSRAuth';
-import { Header } from '../../components/Header';
-import { Button } from '../../components/ui/Button';
-import Router from 'next/router';
-import styles from './styles.module.scss';
-import { AuthContext } from '../../contexts/AuthContext';
+import * as React from "react";
+import { useContext } from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { setupAPIClient } from "../../services/api";
+import { canSSRAuth } from "../../../utils/canSSRAuth";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/ui/Button";
+import Router from "next/router";
+import styles from "./styles.module.scss";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function AulasPage({ classes }) {
   const { studentIdState } = useContext(AuthContext);
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90, editable: false },
+    { field: "id", headerName: "ID", width: 90, editable: false },
     {
-      field: 'name',
-      headerName: 'Nome',
+      field: "name",
+      headerName: "Nome",
       width: 170,
       editable: false,
     },
     {
-      field: 'date',
-      headerName: 'Data',
+      field: "date",
+      headerName: "Data",
       width: 170,
       editable: false,
     },
     {
-      field: 'time',
-      headerName: 'Horário',
+      field: "time",
+      headerName: "Horário",
       width: 170,
       editable: false,
     },
     {
-      field: 'duration',
-      headerName: 'Duração',
+      field: "duration",
+      headerName: "Duração",
       width: 170,
       editable: false,
     },
     {
-      field: 'studentName',
-      headerName: 'Aluno',
+      field: "studentName",
+      headerName: "Aluno",
       width: 170,
       editable: false,
     },
@@ -53,27 +53,26 @@ export default function AulasPage({ classes }) {
       renderCell: (params) => {
         const onClick = (e) => {
           e.stopPropagation();
-  
+
           const api = params.api;
           const thisRow = {};
-  
+
           api
             .getAllColumns()
-            .filter((c) => c.field !== '__check__' && !!c)
+            .filter((c) => c.field !== "__check__" && !!c)
             .forEach(
-              (c) => (thisRow[c.field] = params.getValue(params.id, c.field)),
+              (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
             );
           let id = thisRow.id;
           studentIdState(id);
-          Router.push('/editAula');
+          Router.push("/editAula");
           return console.log(JSON.stringify(thisRow, null, 4));
         };
-  
+
         return <Button onClick={onClick}>Editar</Button>;
-      }
+      },
     },
   ];
-
 
   return (
     <div className={styles.containerCenter}>
@@ -84,53 +83,55 @@ export default function AulasPage({ classes }) {
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        columnVisibilityModel={{ // Para esconder a coluna id
-            id: false
-          } 
-        }
-
+        columnVisibilityModel={{
+          // Para esconder a coluna id
+          id: false,
+        }}
         sx={{
           height: 400,
-          backgroundColor: 'transparent',
-          color: '#FFF',
+          backgroundColor: "transparent",
+          color: "#FFF",
           border: 0,
-          borderRadius: '10px',
-          '& .MuiDataGrid-cell:hover': {
-            color: '#48577E',
-            backgroundColor: '#3AAFA1;'
+          borderRadius: "10px",
+          "& .MuiDataGrid-cell:hover": {
+            color: "#48577E",
+            backgroundColor: "#3AAFA1;",
           },
-          '& .MuiDataGrid-row:hover': {
-            backgroundColor: '#3AAFA1',
-            color: '#FFF'
+          "& .MuiDataGrid-row:hover": {
+            backgroundColor: "#3AAFA1",
+            color: "#FFF",
           },
-          '& .MuiDataGrid-row': {
-            background: '#3AAFA1',
-          }
+          "& .MuiDataGrid-row": {
+            background: "#3AAFA1",
+          },
         }}
       />
-        <Button onClick={handleRegisterLink} style={{ 
-          backgroundColor: '#3A62AF',
-          height: '60px',
-          width: '480px',
-          fontSize: '20px',
-          marginBottom: '50px'
-        }}>
-          Adicionar nova aula
-        </Button> 
-      </div>
+      <Button
+        onClick={handleRegisterLink}
+        style={{
+          backgroundColor: "#3A62AF",
+          height: "60px",
+          width: "480px",
+          fontSize: "20px",
+          marginBottom: "50px",
+        }}
+      >
+        Adicionar nova aula
+      </Button>
+    </div>
   );
 }
 
 function handleRegisterLink() {
-    Router.push('/createAula');
+  Router.push("/createAula");
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-    const apiClient = setupAPIClient(ctx);
-    const classRes = await apiClient.get('/class/detail');
-    return {
-        props: {
-            classes: classRes.data
-        }
-    }
-})
+  const apiClient = setupAPIClient(ctx);
+  const classRes = await apiClient.get("/class/detail");
+  return {
+    props: {
+      classes: classRes.data,
+    },
+  };
+});
--- frontend/src/pages/createAula/index.jsx
+++ frontend/src/pages/createAula/index.jsx
@@ -1,34 +1,38 @@
-import { useState, useContext } from 'react';
-import Head from 'next/head';
-import styles from '../../../styles/Home.module.scss';
-import CustomizedInputs from '../../components/ui/StyledInputs/CustomizedInputs';
-import { Button } from '../../components/ui/Button';
-import Link from 'next/link';
-import { AuthContext } from '../../contexts/AuthContext';
-import { toast } from 'react-toastify';
-import { canSSRAuth } from '../../../utils/canSSRAuth';
-import { Header } from '../../components/Header';
-import { setupAPIClient } from '../../services/api';
-import MenuItem from '@mui/material/MenuItem';
-import Router from 'next/router';
-import InputMask from 'react-input-mask';
-import { useStyleRegistry } from 'styled-jsx';
-
+import { useState, useContext } from "react";
+import Head from "next/head";
+import styles from "../../../styles/Home.module.scss";
+import CustomizedInputs from "../../components/ui/StyledInputs/CustomizedInputs";
+import { Button } from "../../components/ui/Button";
+import Link from "next/link";
+import { AuthContext } from "../../contexts/AuthContext";
+import { toast } from "react-toastify";
+import { canSSRAuth } from "../../../utils/canSSRAuth";
+import { Header } from "../../components/Header";
+import { setupAPIClient } from "../../services/api";
+import MenuItem from "@mui/material/MenuItem";
+import Router from "next/router";
+import InputMask from "react-input-mask";
+import { useStyleRegistry } from "styled-jsx";
 
 export default function RegisterClass({ allStudents }) {
-
   const { registerClass } = useContext(AuthContext);
 
-  const [data, setData] = useState(''); 
-  const [hora, setHora] = useState('');
-  const [duracao, setDuracao] = useState('');
-  const [nomedaAula, setNomeAula] = useState('');
-  const [alunos, setAlunos] = useState('');
+  const [data, setData] = useState("");
+  const [hora, setHora] = useState("");
+  const [duracao, setDuracao] = useState("");
+  const [nomedaAula, setNomeAula] = useState("");
+  const [alunos, setAlunos] = useState("");
 
-  async function handleCreateAula(event){
+  async function handleCreateAula(event) {
     event.preventDefault();
 
-    if(data === '' || hora === '' || duracao === '' || alunos === '' || nomedaAula === '') {
+    if (
+      data === "" ||
+      hora === "" ||
+      duracao === "" ||
+      alunos === "" ||
+      nomedaAula === ""
+    ) {
       toast.error("Preencha os campos!");
       return;
     }
@@ -36,136 +40,134 @@ export default function RegisterClass({ allStudents }) {
     let studentName = "";
 
     allStudents.map((st) => {
-      if (st.id === alunos)
-        studentName = st.name;
+      if (st.id === alunos) studentName = st.name;
     });
 
     let body = {
-        name: nomedaAula,
-        date: data,
-        time: hora,
-        duration: duracao,
-        studentID: alunos,
-        studentName: studentName,
-    }
+      name: nomedaAula,
+      date: data,
+      time: hora,
+      duration: duracao,
+      studentID: alunos,
+      studentName: studentName,
+    };
 
-    console.log(body)
+    console.log(body);
 
     await registerClass(body);
 
-    setData('');
-    setNomeAula('');
-    setHora('');
-    setDuracao('');
-    setAlunos('');
+    setData("");
+    setNomeAula("");
+    setHora("");
+    setDuracao("");
+    setAlunos("");
   }
-  
+
   return (
     <>
-    <Head>
-      <title>Strix - Cadastre uma aula</title>
-    </Head>
-    <Header />
-    <div className={styles.containerCenterRegister}>
-      <div className={styles.login}> 
-        
-        <Link href="/createAula" className={styles.subtitulo}>
-          Registro de Aula
-        </Link>
+      <Head>
+        <title>Strix - Cadastre uma aula</title>
+      </Head>
+      <Header />
+      <div className={styles.containerCenterRegister}>
+        <div className={styles.login}>
+          <Link href="/createAula" className={styles.subtitulo}>
+            Registro de Aula
+          </Link>
 
-        <form onSubmit={handleCreateAula}>
+          <form onSubmit={handleCreateAula}>
+            <InputMask
+              mask="99/99/9999"
+              maskChar=""
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+            >
+              {() => (
+                <CustomizedInputs
+                  size="small"
+                  label={"Data da aula (DD/MM/AAAA)*"}
+                  type={"text"}
+                />
+              )}
+            </InputMask>
 
-          <InputMask
-                      mask="99/99/9999"
-                      maskChar=""
-                      value={data}
-                      onChange={ (e) => setData(e.target.value) }
-          >
-          { () => <CustomizedInputs
-            size='small' 
-            label={'Data da aula (DD/MM/AAAA)*'} 
-            type={'text'} 
-          />}
-          </InputMask>
-          
-          <InputMask
-                      mask="99:99"
-                      maskChar=""
-                      value={hora}
-                      onChange={ (e) => setHora(e.target.value) }
-          >
-            {
-              () => <CustomizedInputs
-              size='small' 
-              label={'Horário (HH:MM)*'} 
-              type={'text'} 
+            <InputMask
+              mask="99:99"
+              maskChar=""
+              value={hora}
+              onChange={(e) => setHora(e.target.value)}
+            >
+              {() => (
+                <CustomizedInputs
+                  size="small"
+                  label={"Horário (HH:MM)*"}
+                  type={"text"}
+                />
+              )}
+            </InputMask>
+
+            <CustomizedInputs
+              size="small"
+              label={"Duração da aula (em minutos)*"}
+              type={"text"}
+              value={duracao}
+              onChange={(e) => setDuracao(e.target.value)}
             />
-            }
-          </InputMask>
 
-          <CustomizedInputs
-            size='small' 
-            label={'Duração da aula (em minutos)*'} 
-            type={'text'} 
-            value={duracao}
-            onChange={ (e) => setDuracao(e.target.value) }
-          />
+            <CustomizedInputs
+              size="small"
+              select
+              label={"Alunos *"}
+              type={"text"}
+              value={alunos}
+              onChange={(e) => {
+                setAlunos(e.target.value);
+              }}
+              sx={{
+                ".MuiSelect-icon": {
+                  color: "#D9D9D9",
+                },
+              }}
+            >
+              {allStudents.map((option) => (
+                <MenuItem key={option.name} value={option.id}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </CustomizedInputs>
 
-          <CustomizedInputs
-            size='small' 
-            select
-            label={'Alunos *'} 
-            type={'text'} 
-            value={alunos}
-            onChange={(e) => {
-              setAlunos(e.target.value);
-            }}
-            sx={{
-              '.MuiSelect-icon':{
-                color: '#D9D9D9'
-              }
-            }}
-          >
-            {allStudents.map((option) => (
-              <MenuItem key={option.name} value={option.id}>
-                {option.name}
-              </MenuItem>
-            ))}
-          </CustomizedInputs>
+            <CustomizedInputs
+              size="small"
+              type={"text"}
+              label={"Nome/descrição da aula *"}
+              multiline
+              rows={3}
+              value={nomedaAula}
+              onChange={(e) => setNomeAula(e.target.value)}
+              variant="outlined"
+              sx={{
+                backgroundColor: "#D9D9D9",
+                borderRadius: "10px",
+                border: "0",
+                marginBottom: "1rem",
+              }}
+            />
 
-          <CustomizedInputs 
-            size='small' 
-            type={"text"} 
-            label={'Nome/descrição da aula *'}
-            multiline
-            rows={3}
-            value={nomedaAula}
-            onChange={ (e) => setNomeAula(e.target.value) }
-            variant="outlined"
-            sx={{
-                backgroundColor: '#D9D9D9',
-                borderRadius: '10px',
-                border: '0',
-                marginBottom: '1rem',
-            }}
-          />
-
-          <p className={styles.msg}>* Campo Obrigatório</p>
-          <Button type='submit'>Cadastrar Aula</Button>
-        </form>
-        
+            <p className={styles.msg}>* Campo Obrigatório</p>
+            <Button type="submit">Cadastrar Aula</Button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
-  const response = await apiClient.get('/listStudents');
+  const response = await apiClient.get("/listStudents");
   return {
-      props: {
-        allStudents: response.data
-      },
-    };
+    props: {
+      allStudents: response.data,
+    },
+  };
 });
