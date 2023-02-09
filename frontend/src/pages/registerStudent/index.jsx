84a02f69ed1a82235c41ea2498918e15e347d800
Pedro Daia Cardoso <pedrodaia.c@gmail.com>
2023-02-09 11:43:49 -0300

    reverting commit

--- frontend/src/pages/registerStudent/index.jsx
+++ frontend/src/pages/registerStudent/index.jsx
@@ -1,192 +1,177 @@
-import { useState, useContext, useEffect } from "react";
-import Head from "next/head";
-import styles from "../../../styles/Home.module.scss";
-import CustomizedInputs from "../../components/ui/StyledInputs/CustomizedInputs";
-import { Button } from "../../components/ui/Button";
-import Link from "next/link";
-import { AuthContext } from "../../contexts/AuthContext";
-import { toast } from "react-toastify";
-import { canSSRAuth } from "../../../utils/canSSRAuth";
-import { Header } from "../../components/Header";
+import { useState, useContext, useEffect } from 'react';
+import Head from 'next/head';
+import styles from '../../../styles/Home.module.scss';
+import CustomizedInputs from '../../components/ui/StyledInputs/CustomizedInputs';
+import { Button } from '../../components/ui/Button';
+import Link from 'next/link';
+import { AuthContext } from '../../contexts/AuthContext';
+import { toast } from 'react-toastify';
+import { canSSRAuth } from '../../../utils/canSSRAuth';
+import { Header } from '../../components/Header';
 import * as cpfValidator from "cpf-cnpj-validator";
-import InputMask from "react-input-mask";
+import InputMask from 'react-input-mask';
 
 export default function RegisterStudent() {
   const { registerStudent } = useContext(AuthContext);
 
-  const [name, setName] = useState("");
-  const [birthDate, setBirthdDate] = useState("");
-  const [phone, setPhone] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState(''); 
+  const [birthDate, setBirthdDate] = useState('');
+  const [phone, setPhone] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
 
-  const [cpfValid, setCpfValid] = useState(true);
+  const [cpfValid, setCpfValid] = useState(true)
 
-  async function handleRegisterStudent(event) {
+  async function handleRegisterStudent(event){
     event.preventDefault();
 
-    if (
-      name === "" ||
-      birthDate === "" ||
-      phone === "" ||
-      cpf === "" ||
-      email === ""
-    ) {
+    if(name === '' || birthDate === '' || phone === '' || cpf === '' || email === '') {
       toast.error("Preencha os campos!");
       //alert('Preencha todos os campos!')
       return;
     }
-    let invalid = false;
+    let invalid = false
 
     if (birthDate.length < 10) {
-      toast.error("Data de nascimento inválida!");
-      invalid = true;
+      toast.error("Data de nascimento inválida!")
+      invalid = true
     }
     if (phone.length < 15) {
-      toast.error("Telefone inválido!");
-      invalid = true;
+      toast.error("Telefone inválido!")
+      invalid = true
     }
     if (!cpfValid) {
-      toast.error("CPF inválido!");
-      invalid = true;
+      toast.error("CPF inválido!")
+      invalid = true
     }
     if (invalid) {
-      return;
+      return
     }
 
+    const user_id = "8675c6c7-ec90-45ab-928b-fb9b60510e81"; // Como só temos um usuário, esse id é único
+    //const user_id = "04b623b0-ccdb-4a32-8a6d-55e1b67ea038";
 
-    const user_id = "50f7bb58-eaa3-4526-ae3a-7e21be655830"; // Como só temos um usuário, esse id é único
+      let data = {
+        name,
+        birthDate,
+        phone,
+        cpf,
+        email,
+        user_id
+      }
 
-
-    let data = {
-      name,
-      birthDate,
-      phone,
-      cpf,
-      email,
-      user_id,
-    };
-
-    await registerStudent(data);
+      await registerStudent(data);
   }
 
   useEffect(() => {
-    setCpfValid(cpfValidator.cpf.isValid(cpf));
-  }, [cpf]);
-
+    setCpfValid(cpfValidator.cpf.isValid(cpf))
+  }, [cpf])
+  
   const validateName = (text) => {
-    let nome = "";
-    nome = nome.concat(text);
-    let tam = nome.length;
-    const letraspermitidas =
-      "ABCEDFGHIJKLMNOPQRSTUVXWYZ abcdefghijklmnopqrstuvxwyzáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ";
+    let nome = ""
+    nome = nome.concat(text)
+    let tam = nome.length
+    const letraspermitidas="ABCEDFGHIJKLMNOPQRSTUVXWYZ abcdefghijklmnopqrstuvxwyzáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ"
     while (tam--) {
-      if (!letraspermitidas.includes(nome.charAt(tam))) {
-        return false;
+      if(!letraspermitidas.includes(nome.charAt(tam))) {
+        return false
       }
     }
-    return true;
-  };
-
+    return true
+  }
+  
   return (
     <>
-      <Head>
-        <title>Strix - Faça seu cadastro</title>
-      </Head>
-      <Header />
-      <div className={styles.containerCenterRegister}>
-        <div className={styles.login}>
-          <Link href="/" className={styles.subtitulo}>
-            Cadastro de aluno
-          </Link>
+    <Head>
+      <title>Strix - Faça seu cadastro</title>
+    </Head>
+    <Header />
+    <div className={styles.containerCenterRegister}>
+      <div className={styles.login}> 
+        
+        <Link href="/" className={styles.subtitulo}>
+          Cadastro de aluno
+        </Link>
 
-          <form onSubmit={handleRegisterStudent}>
-            <CustomizedInputs
-              size="small"
-              label={"Nome completo"}
-              type={"text"}
+        <form onSubmit={handleRegisterStudent}>
+          <CustomizedInputs
+            size='small' 
+            label={'Nome completo'} 
+            type={'text'} 
+            required 
+            value={name}
+            onChange={ (e) => validateName(e.target.value) ? setName(e.target.value) : null }
+          />
+
+          <InputMask 
+            mask="99/99/9999"
+            maskChar=""
+            value={birthDate}
+            onChange={ (e) => setBirthdDate(e.target.value) }
+          >
+          {() => <CustomizedInputs 
+            size='small' 
+            type={"text"} 
+            label={'Data de nascimento'} 
+            required 
+            error={birthDate.length > 0 && birthDate.length < 10}
+          />}
+        </InputMask>
+
+          <InputMask 
+            mask="(99) 99999-9999"
+            maskChar=""
+            value={phone}
+            onChange={ (e) => setPhone(e.target.value) }
+          >
+            {() => <CustomizedInputs 
+              size='small' 
+              type={'text'} 
+              label={'Telefone'} 
               required
-              value={name}
-              onChange={(e) =>
-                validateName(e.target.value) ? setName(e.target.value) : null
-              }
-            />
+              error={phone.length > 0 && phone.length < 15}
+            />}
+          </InputMask>
 
-            <InputMask
-              mask="99/99/9999"
-              maskChar=""
-              value={birthDate}
-              onChange={(e) => setBirthdDate(e.target.value)}
-            >
-              {() => (
-                <CustomizedInputs
-                  size="small"
-                  type={"text"}
-                  label={"Data de nascimento"}
-                  required
-                  error={birthDate.length > 0 && birthDate.length < 10}
-                />
-              )}
-            </InputMask>
-
-            <InputMask
-              mask="(99) 99999-9999"
-              maskChar=""
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            >
-              {() => (
-                <CustomizedInputs
-                  size="small"
-                  type={"text"}
-                  label={"Telefone"}
-                  required
-                  error={phone.length > 0 && phone.length < 15}
-                />
-              )}
-            </InputMask>
-
-            <InputMask
-              mask="999.999.999-99"
-              maskChar=""
-              value={cpf}
-              onChange={(e) => setCpf(e.target.value)}
-            >
-              {() => (
-                <CustomizedInputs
-                  size="small"
-                  type={"text"}
-                  label={"CPF"}
-                  required
-                  error={!cpfValid && cpf.length > 0}
-                />
-              )}
-            </InputMask>
-
-            <CustomizedInputs
-              size="small"
-              type={"email"}
-              label={"E-mail"}
+          <InputMask 
+            mask="999.999.999-99"
+            maskChar=""
+            value={cpf}
+            onChange={ (e) => setCpf(e.target.value) }
+          >
+            {() => <CustomizedInputs
+              size='small' 
+              type={'text'} 
+              label={'CPF'} 
               required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+              error={!cpfValid && cpf.length > 0}
+            />}
+          </InputMask>
 
-            <p className={styles.msg}>* Campo Obrigatório</p>
-            <Button type="submit">Cadastrar</Button>
-          </form>
+          <CustomizedInputs 
+            size='small' 
+            type={'email'} 
+            label={'E-mail'} 
+            required
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
+          />
 
-          {/* <Link href="/" className={styles.text}>
+          <p className={styles.msg}>* Campo Obrigatório</p>
+          <Button type='submit'>Cadastrar</Button> 
+        </form>
+
+        {/* <Link href="/" className={styles.text}>
           Esqueci minha senha
         </Link> */}
-        </div>
       </div>
+    </div>
     </>
-  );
+  )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
   return {
-    props: {},
-  };
-});
+      props: {}
+  }
+})
