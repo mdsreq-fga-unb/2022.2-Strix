0e69727dc9e0058d0377cb34a3560fee5e98b4a1
Pedro Daia Cardoso <pedrodaia.c@gmail.com>
2023-02-09 11:46:52 -0300

    reverting commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,36 +1,34 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
-import { AuthTokenError } from "./errors/AuthTokenError";
-import { signOut } from "../contexts/AuthContext";
+import axios from 'axios';
+import { parseCookies } from 'nookies';
+import { AuthTokenError } from './errors/AuthTokenError';
+import { signOut } from '../contexts/AuthContext';
 
-export function setupAPIClient(ctx = undefined) {
-  let cookies = parseCookies(ctx);
+export function setupAPIClient(ctx = undefined){
+    let cookies = parseCookies(ctx);
 
-  const api = axios.create({
-    baseURL: "https://strix-backend.onrender.com",
-    headers: {
-      Authorization: `Bearer ${cookies["@nextauth.token"]}`,
-    },
-  });
-
-  api.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response.status === 401) {
-        // Qualquer erro 401 (não autorizado) devemos deslogar o usuário
-        if (typeof window !== undefined) {
-          // Deslogar o usuário
-          signOut();
-        } else {
-          return Promise.reject(new AuthTokenError());
+    const api = axios.create({
+        //baseURL: 'http://localhost:3333',
+        baseURL: 'https://api-strix.onrender.com',
+        headers: {
+            Authorization: `Bearer ${cookies['@nextauth.token']}`
         }
-      }
+    })
 
-      return Promise.reject(error);
-    }
-  );
+    api.interceptors.response.use(response => {
+        return response;
+    }, (error) => {
+        if(error.response.status === 401){
+            // Qualquer erro 401 (não autorizado) devemos deslogar o usuário
+            if(typeof window !== undefined){
+                // Deslogar o usuário
+                signOut();
+            } else {
+                return Promise.reject(new AuthTokenError());
+            }
+        }
 
-  return api;
+        return Promise.reject(error);
+    })
+
+    return api;
 }
